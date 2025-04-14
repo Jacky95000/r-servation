66a426edcaebb0bcadea1b29c4e65362f96b34d8
Jacky95000 <jacky.bonnot@lapiscine.pro>
2025-04-14 14:28:38 +0200

    reservation

--- reservation.model.php
+++ reservation.model.php
@@ -1,5 +1,5 @@
 <?php
-
+// class pour la réservation
 class Reservation {
     public $name;
     public $place;
@@ -11,9 +11,9 @@ class Reservation {
     public $bookedAt;
     public $cleaningOption;
 }
-
+// création de l'objet
 $reservation = new Reservation ();
-
+// valeurs pour créer l'objet
 $reservation->name = "Jacky Bonnot";
 $reservation->place = "Paris";
 $reservation->startDate = new DateTime("25-04-15");
@@ -21,6 +21,7 @@ $reservation->endDate = new DateTime("25-05-17");
 $reservation->cleaningOption = true;
 $reservation->nightPrice = 1000;
 
+// Calcul qui va permettre de connaitre le prix par nuit
 $totalPrice = (($reservation->endDate->getTimestamp() - $reservation->startDate->getTimestamp()) / (3600 * 24) * $reservation->nightPrice) + 5000;
 
 $reservation->totalPrice = $totalPrice;
